42366a9579076eaa11aa1336bca39ab752b3c662
Jakub Jarecki <jakub.jarecki@student.uj.edu.pl>
2024-03-20 22:55:32 +0100

    dodajemy plik 4b.py

---
+++ 4b.py
@@ -0,0 +1,17 @@
+a="Common Sequence Operations"
+x = a[7:15]
+print(x)
+m_x='.'.join(x)
+print(m_x)
+
+b="Mutable Sequence Types"
+y=b[-5:]
+print(y)
+
+c="Mutable Sequence Types"
+z=c[::2]
+print(z)
+
+d="Mutable Sequence Types"
+db=d[::-1]
+print(db)
